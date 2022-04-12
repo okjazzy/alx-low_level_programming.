fa363fa1f87d00cd34ec21ce545bbee66be842ad
Okiti <ibukunokiti@gmail.com>
2022-04-13 00:14:33 +0100

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0.
  */
-void times_table(int n)
+void times_table(void)
 {
 int a;
 int b;
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,5 +12,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
+void print_times_table(void);
 #endif /* _MAIN_H_ */
