45afaa01a08c5616a856534f6b349fd0752b489a
Okiti <ibukunokiti@gmail.com>
2022-04-13 22:27:12 +0100

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,18 +9,17 @@ int main(void)
 {
 int i;
 
-printf("infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
 i = 0;
 
 /*
  * while (i < 10).
  * {
- * infinite loop - No increase of variable
+ * Infinite loop - No increase of variable
  * putchar(i);
- * }
- */
-
+}
+*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
