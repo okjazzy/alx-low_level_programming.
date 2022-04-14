baa691e70a3c2051a4fd3215a53e7665a734e7ba
Okiti <ibukunokiti@gmail.com>
2022-04-14 12:48:13 +0100

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
  * print_numbers - print numbers
  * Return: Always 0.
  */
+
 void print_numbers(void)
 {
 int i;
 
 i = 0;
-while (1 < 10)
+while (i < 10)
 {
-_putchar (1 + '0');
+_putchar (i +'0');
 i++;
 }
 
