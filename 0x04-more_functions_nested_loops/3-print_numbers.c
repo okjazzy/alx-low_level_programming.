00a3a46b9c28ed27711bf4a2cfd4a9e3ec2b886c
Okiti <ibukunokiti@gmail.com>
2022-04-14 13:50:37 +0100

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,19 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
 {
 int x;
 
-for (x = 48; x < 58; x++)
+for (x = 0; x < 9; x++)
 {
-_putchar(x);
+_putchar(x + '0');
 }
-_putchar(10);
+
+_putchar('\n');
 }
+
