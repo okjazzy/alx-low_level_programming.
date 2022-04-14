f31bdee1a5385ee924473ec485e66b23dbf2c054
Okiti <ibukunokiti@gmail.com>
2022-04-14 09:20:08 +0100

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - evaluate if letter is uppercase.
+ * @c: print int
+ * REturn: Always 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+retrun (1);
+}
+
+else
+{
+return (0);
+}
+
+}
