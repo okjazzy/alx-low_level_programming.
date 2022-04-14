95aeb285b9d1def2b661a1f9d1fea496afaf56b7
Okiti <ibukunokiti@gmail.com>
2022-04-14 15:19:04 +0100

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,6 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+
 if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 {
 if (month >= 2 && day >= 60)
@@ -34,4 +35,5 @@ printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
+
 }
