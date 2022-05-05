772c95cc0cd664cfef663df8983550bdc84743f5
okjazzy <ibukunokiti@gmail.com>
2022-05-05 07:38:38 +0100

    Task

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -9,23 +9,22 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int j;
+int j;
 
-	while (*s != '\0') /*Declaring WHILE*/
-	{
-		j = 0;
-		while (accept[j] != '\0')  /*Evaluating *accept*/
-		{
-			if (*s == accept[j])
-			{
-				return (s);
-			}
+while (*s != '\0') /*Declaring WHILE*/
+{
+j = 0;
+while (accept[j] != '\0')  /*Evaluating *accept*/
+{
+if (*s == accept[j])
+{
+return (s);
+}
 
-			j++; /*add j+1*/
-		}
+j++; /*add j+1*/
+}
 
-		s++; /*add s+1*/
-	}
-	return (0);
-
+s++; /*add s+1*/
+}
+return (0);
 }
