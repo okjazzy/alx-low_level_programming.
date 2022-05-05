90253b8fd56caded081dab8d0474428c78dce405
okjazzy <ibukunokiti@gmail.com>
2022-05-05 07:45:47 +0100

    Task

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -14,17 +14,17 @@ int i, j;
 
 for (i = 0; haystack[i] > '\0'; i++)
 {
-	for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
-	{
-		if (haystack[j] != needle[j - i])
-		{
-			break;
-		}
-	}
-	if (needle[j - i] == '\0')
-	{
-		return (haystack + i);
-	}
+for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
+{
+if (haystack[j] != needle[j - i])
+{
+break;
+}
+}
+if (needle[j - i] == '\0')
+{
+return (haystack + i);
+}
 }
 return (0);
 }
