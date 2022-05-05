0a3f4ab386b1bc6074baf11ace6d4d6d613f7df5
okjazzy <ibukunokiti@gmail.com>
2022-05-05 07:08:12 +0100

    Task

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -17,10 +17,10 @@ int i;
 
 for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
-	if (s1[i] != s2[i])
-	{
-		return (s1[i] - s2[i]);
-	}
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
 }
 return (0);
 }
--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -10,22 +10,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, cmpt = 0;
+int i, j, cmpt = 0;
 
-	for (i = 0; s[i] >= '\0'; i++)
-	{
-		for (j = 0; accept[j] > '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				cmpt++;
-				break;
-			}
-		}
-		if (accept[j] == '\0')
-		{
-			break;
-		}
-	}
-	return (cmpt);
+for (i = 0; s[i] >= '\0'; i++)
+{
+for (j = 0; accept[j] > '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+cmpt++;
+break;
+}
+}
+if (accept[j] == '\0')
+{
+break;
+}
+}
+return (cmpt);
 }
