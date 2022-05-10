f1196a325150d4535eef39fc87869a79cd5c738d
okjazzy <ibukunokiti@gmail.com>
2022-05-10 03:38:11 +0100

    Task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,29 +1,57 @@
 #include "main.h"
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * _memcpy - copy memory data from src to dest
+ * @dest: memory destination
+ * @src: memory source
+ * @n: size of new memory
  */
-int main(int argc, char *argv[])
+void _memcpy(void *dest, void *src, size_t n)
 {
-unsigned long mul;
-int i, j;
-if (argc != 3)
-{ printf("Error\n");
-exit(98); }
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (argv[i][j] > 57 || argv[i][j] < 48)
-{  printf("Error\n");
-exit(98); }
+size_t i;
+char *csrc = (char *)src;
+char *cdest = (char *)dest;
+
+for (i = 0; i < n; i++)
+cdest[i] = csrc[i];
 }
 
+/**
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: array length
+ * @old_size: size of old memory
+ * @new_size: size of new memory
+ * Return: pointer to new memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+void *newPtr;
+if (new_size == 0)
+{
+if (ptr != NULL)
+free(ptr);
+return (NULL);
 }
-mul = atol(argv[1]) * atol(argv[2]);
-printf("%lu\n", mul);
+else if (!ptr)
+{
+	return (malloc(new_size));
+}
+else if (new_size <= old_size)
+{
+	return (ptr);
+}
+else
+{
+newPtr = malloc(new_size);
+if (newPtr)
+{
+_memcpy(newPtr, ptr, old_size);
+free(ptr);
+}
+return (newPtr);
+}
+
 return (0);
 }
